0b2be8d37d8fbe11c84e15d94eff3715da9eaf3f
Tú Văn Trần <tu.van.tran@mgm-tp.com>
2024-04-20 15:59:10 +0700

    Updated

--- spring-framework/core-technologies/ioc-container/container-overview/src/main/java/com/franktranvantu/springframework/coretechnologies/ioccontainer/containeroverview/model/Employee.java
+++ spring-framework/core-technologies/ioc-container/container-overview/src/main/java/com/franktranvantu/springframework/coretechnologies/ioccontainer/containeroverview/model/Employee.java
@@ -7,4 +7,56 @@ public class Employee {
     private String lastName;
     private Date dateOfBirth;
     private String phoneNumber;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, Date dateOfBirth, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
--- spring-framework/core-technologies/ioc-container/container-overview/src/main/java/com/franktranvantu/springframework/coretechnologies/ioccontainer/containeroverview/model/Team.java
+++ spring-framework/core-technologies/ioc-container/container-overview/src/main/java/com/franktranvantu/springframework/coretechnologies/ioccontainer/containeroverview/model/Team.java
@@ -3,4 +3,36 @@ package com.franktranvantu.springframework.coretechnologies.ioccontainer.contain
 public class Team {
     private String name;
     private String location;
+
+    public Team() {
+    }
+
+    public Team(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
