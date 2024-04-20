9eee76bfb2d3a4e517d6ef91622cfa708d452af7
Tú Văn Trần <tu.van.tran@mgm-tp.com>
2024-04-20 13:14:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+The repository contains all related topics about spring-framework
+
+![spring-framework.jpg](spring-framework.jpg)
---
+++ spring-mvc/src/main/java/com/franktranvantu/springmvc/App.java
@@ -0,0 +1,7 @@
+package com.franktranvantu.springmvc;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
